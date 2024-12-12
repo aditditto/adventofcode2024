f25b7294cd16ea13a94e749ad6b65c7e02aa0be4
Aditya Bimawan <bimawan.aditya@moneyforward.co.jp>
2024-12-12 12:37:57 +0900

    day 3 part 2

--- src/03.kt
+++ src/03.kt
@@ -6,11 +6,18 @@ fun main() {
 
 fun first() {
     var sum = 0
+    var isMulOn = true
     File("resources/03.in").forEachLine { line ->
-        val matches = Regex("""mul\(([0-9]+),([0-9]+)\)""").findAll(line)
+        val matches = Regex("""mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)""").findAll(line)
         for (match in matches) {
-            val (a, b) = match.destructured
-            sum += a.toInt() * b.toInt()
+            if (match.value == "do()") {
+                isMulOn = true
+            } else if (match.value == "don't()") {
+                isMulOn = false
+            } else if (isMulOn) {
+                val (a, b) = match.destructured
+                sum += a.toInt() * b.toInt()
+            }
         }
     }
     println(sum)
