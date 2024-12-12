2043fa16646623a445334a875f9a887acb11fc29
Aditya Bimawan <bimawan.aditya@moneyforward.co.jp>
2024-12-12 12:39:00 +0900

    split day3

--- src/03.kt
+++ src/03.kt
@@ -2,9 +2,22 @@ import java.io.File
 
 fun main() {
     first()
+    second()
 }
 
 fun first() {
+    var sum = 0
+    File("resources/03.in").forEachLine { line ->
+        val matches = Regex("""mul\(([0-9]+),([0-9]+)\)""").findAll(line)
+        for (match in matches) {
+            val (a, b) = match.destructured
+            sum += a.toInt() * b.toInt()
+        }
+    }
+    println(sum)
+}
+
+fun second() {
     var sum = 0
     var isMulOn = true
     File("resources/03.in").forEachLine { line ->
