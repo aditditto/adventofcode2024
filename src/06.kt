915d6750579ac76dcce1a50ce9f1a4bcd4342650
Aditya Bimawan <bimawan.aditya@moneyforward.co.jp>
2024-12-20 01:31:45 +0900

    day 6

--- src/06.kt
+++ src/06.kt
@@ -12,19 +12,67 @@ fun main() {
     println(first(map))
 }
 
+enum class Direction(val icon: Char, val deltaI: Int, val deltaJ: Int) {
+    UP('^', -1, 0) {
+        override fun nextDirection() = RIGHT
+    },
+    DOWN('v', 1, 0) {
+        override fun nextDirection() = LEFT
+    },
+    LEFT('<', 0, -1) {
+        override fun nextDirection() = UP
+    },
+    RIGHT('>', 0, 1) {
+        override fun nextDirection() = DOWN
+    };
+
+    abstract fun nextDirection(): Direction
+}
+
 fun first(map: MutableList<MutableList<Char>>): Int {
-    val charIcons = listOf('^', '>', '<', 'v')
-    var charJ: Int
-    var charI: Int
+    val icons = Direction.entries.map { it.icon }
+    var iconI = 0
+    var iconJ = 0
+    var sum = 1
     for (i in map.indices) {
         for (j in map[i].indices) {
-            if (charIcons.contains(map[i][j])) {
-                charI = i
-                charJ = j
+            if (icons.contains(map[i][j])) {
+                iconI = i
+                iconJ = j
             }
         }
     }
 
+    var currentDirection = Direction.entries.find { it.icon == map[iconI][iconJ] }!!
+    val blockedCell = '#'
+    val newCell = '.'
+    val visitedCell = 'X'
+    val mapLimitI = map.size
+    val mapLimitJ = map[0].size
+    while (true) {
+        val nextCellI = iconI + currentDirection.deltaI
+        val nextCellJ = iconJ + currentDirection.deltaJ
+        if (nextCellI < 0 || nextCellJ < 0 || nextCellI == mapLimitI || nextCellJ == mapLimitJ) {
+            break
+        }
 
-    return -0
+        val nextCell = map[nextCellI][nextCellJ]
+        if (nextCell == blockedCell) {
+            currentDirection = currentDirection.nextDirection()
+            map[iconI][iconJ] = currentDirection.icon
+        } else {
+            if (nextCell == newCell) {
+                sum++
+            }
+            map[iconI][iconJ] = visitedCell
+            map[nextCellI][nextCellJ] = currentDirection.icon
+            iconI = nextCellI
+            iconJ = nextCellJ
+        }
+    }
+    for (line in map) {
+        println(line)
+    }
+
+    return sum
 }
